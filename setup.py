5f26239a1bb517fbb5c03d3569f73d04d0c924d3
Alexey Vasilyev <alexa.infra@gmail.com>
2014-12-29 19:56:19 +0100

    separate libraries

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ import os
 import platform as plat
 import sys
 import fnmatch
+from distutils.errors import *
+from distutils import log
+from distutils import sysconfig
 
 from setuptools import Extension, setup, find_packages
 from distutils.command.build_ext import build_ext
+from distutils.command.build_clib import build_clib
 
 def recursive_glob(treeroot, pattern):
     results = []
@@ -27,14 +31,14 @@ STB_SRC = glob.glob(os.path.join(STB_PATH, '*.c'))
 JPEG_COMPRESSOR_SRC = glob.glob(os.path.join(JPEG_COMPRESSOR_PATH, '*.cpp'))
 BOOST_PYTHON_SRC = recursive_glob(os.path.join(BOOST_PYTHON_PATH, 'libs', 'python', 'src'), '*.cpp')
 
-SRC = MAIN_SRC + STB_SRC + JPEG_COMPRESSOR_SRC + BOOST_PYTHON_SRC
+SRC = MAIN_SRC
 
 INC_DIRS = [SRC_PATH, THIRDPARTY_PATH, STB_PATH, JPEG_COMPRESSOR_PATH, BOOST_PYTHON_PATH]
 DEF_OPTS = [('BOOST_PYTHON_SOURCE', None),
             ('BOOST_MULTI_INDEX_DISABLE_SERIALIZATION', None),
             ('BOOST_PYTHON_STATIC_LIB', None)]
 PYVERSION = sys.version[0] + sys.version[2]
-LIBS = []
+LIBS = ['stb', 'jpeg-compressor', 'boost-python']
 ARCH = plat.architecture()[0]
 ARCH_64 = ARCH == '64bit'
 WIN32 = sys.platform == 'win32'
@@ -60,9 +64,73 @@ class pil_lite_build_ext(build_ext):
                 e.extra_compile_args = ['/EHsc']
         build_ext.build_extensions(self)
 
+class pil_build_clib(build_clib):
+    def boost_python(self):
+        compiler = self.compiler.compiler_type
+        MSVC = compiler == 'msvc'
+        MINGW = compiler == 'mingw32'
+        defs = []
+        defs += DEF_OPTS
+        if WIN32:
+            defs += [('MS_NO_COREDLL', None), ('Py_ENABLE_SHARED', None)]
+            if MSVC:
+                defs += [('WIN32', None), ('HAVE_ROUND', None)]
+            if ARCH_64 and not MSVC:
+                defs += [('MS_WIN64', None)]
+        py_include = sysconfig.get_python_inc()
+        plat_py_include = sysconfig.get_python_inc(plat_specific=1)
+        inc_dir = [BOOST_PYTHON_PATH, py_include]
+        if plat_py_include != py_include:
+            inc_dir += [plat_py_include]
+        BOOST_PYTHON_CFG = {'sources':BOOST_PYTHON_SRC,
+                            'macros':defs,
+                            'include_dirs':inc_dir}
+        if MSVC:
+            BOOST_PYTHON_CFG['extra_compile_args'] = ['/EHsc']
+        BOOST_PYTHON_LIB = ('boost-python', BOOST_PYTHON_CFG)
+        self.libraries.append(BOOST_PYTHON_LIB)
+
+    def build_libraries(self, libraries):
+        self.boost_python()
+        for (lib_name, build_info) in libraries:
+            sources = build_info.get('sources')
+            if sources is None or not isinstance(sources, (list, tuple)):
+                raise DistutilsSetupError(
+                       "in 'libraries' option (library '%s'), "
+                       "'sources' must be present and must be "
+                       "a list of source filenames" % lib_name)
+            sources = list(sources)
+
+            log.info("building '%s' library", lib_name)
+
+            # First, compile the source code to object files in the library
+            # directory.  (This should probably change to putting object
+            # files in a temporary build directory.)
+            macros = build_info.get('macros')
+            include_dirs = build_info.get('include_dirs')
+            extra_args = build_info.get('extra_compile_args', [])
+            objects = self.compiler.compile(sources,
+                                            output_dir=self.build_temp,
+                                            macros=macros,
+                                            include_dirs=include_dirs,
+                                            extra_postargs=extra_args,
+                                            debug=self.debug)
+
+            # Now "link" the object files together into a static library.
+            # (On Unix at least, this isn't really linking -- it just
+            # builds an archive.  Whatever.)
+            self.compiler.create_static_lib(objects, lib_name,
+                                            output_dir=self.build_clib,
+                                            debug=self.debug)
+
+STB_LIB = ('stb', {'sources':STB_SRC})
+JPEG_COMPRESSOR_LIB = ('jpeg-compressor', {'sources':JPEG_COMPRESSOR_SRC})
+#
+
 mod = Extension('PilLiteExt', sources=SRC, include_dirs=INC_DIRS, define_macros=DEF_OPTS, libraries=LIBS)
 
 setup(name='PilLite',
    ext_modules=[mod],
    packages=find_packages(),
-   cmdclass={'build_ext':pil_lite_build_ext})
+   libraries=[STB_LIB, JPEG_COMPRESSOR_LIB],
+   cmdclass={'build_ext':pil_lite_build_ext, 'build_clib':pil_build_clib})
