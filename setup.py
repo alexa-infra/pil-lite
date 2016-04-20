d9e654b16b7eec92719d85b34e535f35a36bf9e1
Alexey Vasilyev <alexa.infra@gmail.com>
2016-04-20 15:37:22 +0200

    removing boost.python

--- setup.py
+++ setup.py
@@ -16,43 +16,30 @@ ARCH = plat.architecture()[0]
 ARCH_64 = ARCH in ["x86_64", "64bit"]
 WIN32 = sys.platform == 'win32'
 
-def recursive_glob(treeroot, pattern):
-    results = []
-    for base, dirs, files in os.walk(treeroot):
-        goodfiles = fnmatch.filter(files, pattern)
-        results.extend(os.path.join(base, f) for f in goodfiles)
-    return results
-
 def _read(file):
     with open(file, 'rb') as f:
-        return str(f.read())
+        return f.read().decode('utf-8')
 
 SRC_PATH = 'src'
 THIRDPARTY_PATH = os.path.join(SRC_PATH, 'thirdparty')
 
 STB_PATH = os.path.join(THIRDPARTY_PATH, 'stb')
 JPEG_COMPRESSOR_PATH = os.path.join(THIRDPARTY_PATH, 'jpeg-compressor')
-BOOST_PYTHON_PATH = os.path.join(THIRDPARTY_PATH, 'boost-python')
 
 MAIN_SRC = glob.glob(os.path.join(SRC_PATH, '*.cpp'))
 STB_SRC = glob.glob(os.path.join(STB_PATH, '*.c'))
 JPEG_COMPRESSOR_SRC = glob.glob(os.path.join(JPEG_COMPRESSOR_PATH, '*.cpp'))
-BOOST_PYTHON_SRC = recursive_glob(os.path.join(BOOST_PYTHON_PATH, 'libs',
-                                               'python', 'src'), '*.cpp')
 
 SRC = MAIN_SRC
 
-INC_DIRS = [SRC_PATH, THIRDPARTY_PATH, STB_PATH, JPEG_COMPRESSOR_PATH,
-            BOOST_PYTHON_PATH]
-DEF_OPTS = [('BOOST_PYTHON_SOURCE', None),
-            ('BOOST_MULTI_INDEX_DISABLE_SERIALIZATION', None),
-            ('BOOST_PYTHON_STATIC_LIB', None)]
+INC_DIRS = [SRC_PATH, THIRDPARTY_PATH, STB_PATH, JPEG_COMPRESSOR_PATH]
+DEF_OPTS = []
 PYVERSION = sys.version[0] + sys.version[2]
-LIBS = ['stb', 'jpeg-compressor', 'boost-python']
+LIBS = ['stb', 'jpeg-compressor']
 if WIN32:
     LIBS += ['python' + PYVERSION]
 
-def boost_python_defines(compiler):
+def python_defines(compiler):
     MSVC = compiler == 'msvc'
     MINGW = compiler == 'mingw32'
     defs = []
@@ -70,7 +57,7 @@ class pil_lite_build_ext(build_ext):
         MSVC = compiler == 'msvc'
         MINGW = compiler == 'mingw32'
         defs = []
-        defs += boost_python_defines(compiler)
+        defs += python_defines(compiler)
         for e in self.extensions:
             e.define_macros += defs
             if MSVC:
@@ -80,30 +67,7 @@ class pil_lite_build_ext(build_ext):
         build_ext.build_extensions(self)
 
 class pil_build_clib(build_clib):
-    def add_boost_python(self):
-        compiler = self.compiler.compiler_type
-        MSVC = compiler == 'msvc'
-        MINGW = compiler == 'mingw32'
-        defs = []
-        defs += DEF_OPTS
-        defs += boost_python_defines(compiler)
-        py_include = sysconfig.get_python_inc()
-        plat_py_include = sysconfig.get_python_inc(plat_specific=1)
-        inc_dir = [BOOST_PYTHON_PATH, py_include]
-        if plat_py_include != py_include:
-            inc_dir += [plat_py_include]
-        BOOST_PYTHON_CFG = {'sources':BOOST_PYTHON_SRC,
-                            'macros':defs,
-                            'include_dirs':inc_dir}
-        if MSVC:
-            BOOST_PYTHON_CFG['extra_compile_args'] = ['/EHsc']
-        if MINGW:
-            BOOST_PYTHON_CFG['extra_compile_args'] = ['-Wno-unused-local-typedefs']
-        BOOST_PYTHON_LIB = ('boost-python', BOOST_PYTHON_CFG)
-        self.libraries.append(BOOST_PYTHON_LIB)
-
     def build_libraries(self, libraries):
-        self.add_boost_python()
         for (lib_name, build_info) in libraries:
             sources = build_info.get('sources')
             if sources is None or not isinstance(sources, (list, tuple)):
--- src/py_image.cpp
+++ src/py_image.cpp
@@ -1,40 +1,96 @@
 #include <cmath>
-#include <boost/python.hpp>
+#include <Python.h>
 #include "image.h"
 
-using namespace boost::python;
 using namespace inf;
 
-Image* openImage(object& python_file)
+typedef struct {
+    PyObject_HEAD
+    Image* image;
+} ImagingObject;
+
+extern PyTypeObject Imaging_Type;
+
+PyObject* PyImagingNew(Image* imOut)
 {
-    object py_read = getattr(python_file, "read", object());
-    if (py_read == object()) {
-        PyErr_SetString(PyExc_AttributeError,
-            "That Python file object has no 'read' attribute");
-        throw_error_already_set();
+    ImagingObject* imagep;
+
+    if (!imOut)
+        return NULL;
+
+    imagep = PyObject_New(ImagingObject, &Imaging_Type);
+    if (imagep == NULL) {
+        delete imOut;
+        return NULL;
     }
 
-    object read_buffer = py_read();
-    const void* read_buffer_data;
-    Py_ssize_t n_read;
-    if (PyObject_AsReadBuffer(read_buffer.ptr(),
-        &read_buffer_data, &n_read) == -1) {
-        PyErr_SetString(PyErr_Occurred(),
-            "The method 'read' of the Python file object "
-            "did not return a buffer.");
-        throw_error_already_set();
-    }
-    Image* image = new Image((u8*)read_buffer_data, n_read);
-    return image;
+    imagep->image = imOut;
+
+    return (PyObject*) imagep;
 }
 
-void writeImageJpeg(Image* img, object& python_file)
+static void _dealloc(ImagingObject* imagep)
 {
-    object py_write = getattr(python_file, "write", object());
-    if (py_write == object()) {
+    delete imagep->image;
+    PyObject_Del(imagep);
+}
+
+#define PyImaging_Check(op) (Py_TYPE(op) == &Imaging_Type)
+
+Image* PyImaging_AsImaging(PyObject *op)
+{
+    if (!PyImaging_Check(op)) {
+        PyErr_BadInternalCall();
+        return NULL;
+    }
+
+    return ((ImagingObject *)op)->image;
+}
+
+static PyObject* openImage(PyObject* self, PyObject* args)
+{
+    PyObject* python_file;
+
+    if (!PyArg_ParseTuple(args, "O", &python_file))
+        return NULL;
+
+    PyObject* py_read = PyObject_GetAttrString(python_file, "read"); 
+    if (py_read == NULL) {
+        PyErr_SetString(PyExc_AttributeError,
+            "That Python file object has no 'read' attribute");
+        return NULL;
+    }
+    PyObject* read_buffer = PyObject_CallObject(py_read, NULL);
+
+    const void* read_buffer_data;
+    Py_ssize_t n_read;
+    if (PyObject_AsReadBuffer(read_buffer,
+        &read_buffer_data, &n_read) == -1) {
+            PyErr_SetString(PyErr_Occurred(),
+                "The method 'read' of the Python file object "
+                "did not return a buffer.");
+            return NULL;
+    }
+
+    Image* image = new Image((u8*)read_buffer_data, (u32)n_read);
+    return PyImagingNew(image);
+}
+
+static PyObject* writeImageJpeg(PyObject* self, PyObject* args)
+{
+    PyObject* imgobj;
+    PyObject* python_file;
+
+    if (!PyArg_ParseTuple(args, "OO", &imgobj, &python_file))
+        return NULL;
+
+    Image* img = PyImaging_AsImaging(imgobj);
+
+    PyObject* py_write = PyObject_GetAttrString(python_file, "write"); 
+    if (py_write == NULL) {
         PyErr_SetString(PyExc_AttributeError,
             "That Python file object has no 'write' attribute");
-        throw_error_already_set();
+        return NULL;
     }
     ImageCompressed* compress = ImageCompressed::Jpeg(*img, 100);
 #if PY_VERSION_HEX < 0x03000000
@@ -42,18 +98,32 @@ void writeImageJpeg(Image* img, object& python_file)
 #else
     PyObject* bufObj = PyMemoryView_FromMemory((char*)compress->buffer(), compress->size(), PyBUF_READ);
 #endif
-    object buf = object(boost::python::handle<PyObject>(bufObj));
-    py_write(buf);
+    PyObject* callArgs = PyTuple_New(1);
+    PyTuple_SetItem(callArgs, 0, bufObj);
+    PyObject_CallObject(py_write, callArgs);
+    Py_DECREF(callArgs);
+
     delete compress;
+
+    Py_INCREF(Py_None);
+    return Py_None;
 }
 
-void writeImagePng(Image* img, object& python_file)
+static PyObject* writeImagePng(PyObject* self, PyObject* args)
 {
-    object py_write = getattr(python_file, "write", object());
-    if (py_write == object()) {
+    PyObject* imgobj;
+    PyObject* python_file;
+
+    if (!PyArg_ParseTuple(args, "OO", &imgobj, &python_file))
+        return NULL;
+
+    Image* img = PyImaging_AsImaging(imgobj);
+
+    PyObject* py_write = PyObject_GetAttrString(python_file, "write"); 
+    if (py_write == NULL) {
         PyErr_SetString(PyExc_AttributeError,
             "That Python file object has no 'write' attribute");
-        throw_error_already_set();
+        return NULL;
     }
     ImageCompressed* compress = ImageCompressed::Png(*img);
 #if PY_VERSION_HEX < 0x03000000
@@ -61,22 +131,145 @@ void writeImagePng(Image* img, object& python_file)
 #else
     PyObject* bufObj = PyMemoryView_FromMemory((char*)compress->buffer(), compress->size(), PyBUF_READ);
 #endif
-    object buf = object(boost::python::handle<PyObject>(bufObj));
-    py_write(buf);
+    PyObject* callArgs = PyTuple_New(1);
+    PyTuple_SetItem(callArgs, 0, bufObj);
+    PyObject_CallObject(py_write, callArgs);
+    Py_DECREF(callArgs);
+
     delete compress;
+
+    Py_INCREF(Py_None);
+    return Py_None;
 }
 
-BOOST_PYTHON_MODULE(PilLiteExt)
+static PyObject* img_resize(PyObject* self, PyObject* args)
 {
-    def("openImage", &openImage, return_value_policy<manage_new_object>());
-    def("writeImageJpeg", &writeImageJpeg);
-    def("writeImagePng", &writeImagePng);
-    class_<Image>("Image", no_init)
-        .add_property("isOk", &Image::isOk)
-        .add_property("width", &Image::width)
-        .add_property("height", &Image::height)
-        .add_property("componentCount", &Image::componentCount)
-        .add_property("failureReason", &Image::failureReason)
-        .def("resize", &Image::resize, return_value_policy<manage_new_object>())
-        ;
+    int width;
+    int height;
+
+    if (!PyArg_ParseTuple(args, "ii", &width, &height))
+        return NULL;
+
+    Image* img = PyImaging_AsImaging(self);
+
+    Image* newImg = img->resize(width, height);
+
+    return PyImagingNew(newImg);
 }
+
+static PyObject* _getattr_is_ok(ImagingObject* self, void* closure)
+{
+    return PyBool_FromLong(self->image->isOk());
+}
+
+static PyObject* _getattr_width(ImagingObject* self, void* closure)
+{
+    return PyLong_FromUnsignedLong(self->image->width());
+}
+
+static PyObject* _getattr_height(ImagingObject* self, void* closure)
+{
+    return PyLong_FromUnsignedLong(self->image->height());
+}
+
+static PyObject* _getattr_components(ImagingObject* self, void* closure)
+{
+    return PyLong_FromUnsignedLong(self->image->componentCount());
+}
+
+static PyObject* _getattr_failreason(ImagingObject* self, void* closure)
+{
+    return PyUnicode_FromString(self->image->failureReason());
+}
+
+static struct PyGetSetDef getsetters[] = {
+    { "isOk",           (getter) _getattr_is_ok },
+    { "width",          (getter) _getattr_width },
+    { "height",         (getter) _getattr_height },
+    { "componentCount", (getter) _getattr_components },
+    { "failureReason",  (getter) _getattr_failreason },
+    { NULL }
+};
+
+static struct PyMethodDef methods[] = {
+    { "resize", img_resize, METH_VARARGS },
+    { NULL, NULL }
+};
+
+static PyTypeObject Imaging_Type = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    "Imaging",                /*tp_name*/
+    sizeof(ImagingObject),    /*tp_size*/
+    0,                        /*tp_itemsize*/
+    /* methods */
+    (destructor)_dealloc,       /*tp_dealloc*/
+    0,                          /*tp_print*/
+    0,                          /*tp_getattr*/
+    0,                          /*tp_setattr*/
+    0,                          /*tp_compare*/
+    0,                          /*tp_repr*/
+    0,                          /*tp_as_number */
+    0,                          /*tp_as_sequence */
+    0,                          /*tp_as_mapping */
+    0,                          /*tp_hash*/
+    0,                          /*tp_call*/
+    0,                          /*tp_str*/
+    0,                          /*tp_getattro*/
+    0,                          /*tp_setattro*/
+    0,                          /*tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT,         /*tp_flags*/
+    0,                          /*tp_doc*/
+    0,                          /*tp_traverse*/
+    0,                          /*tp_clear*/
+    0,                          /*tp_richcompare*/
+    0,                          /*tp_weaklistoffset*/
+    0,                          /*tp_iter*/
+    0,                          /*tp_iternext*/
+    methods,                    /*tp_methods*/
+    0,                          /*tp_members*/
+    getsetters,                 /*tp_getset*/
+};
+
+static PyMethodDef functions[] = {
+    {"openImage", (PyCFunction)openImage, METH_VARARGS},
+    {"writeImageJpeg", (PyCFunction)writeImageJpeg, METH_VARARGS},
+    {"writeImagePng", (PyCFunction)writeImagePng, METH_VARARGS},
+    {NULL, NULL}
+};
+
+static int setup_module(PyObject* m) {
+    PyObject* d = PyModule_GetDict(m);
+
+    /* Ready object types */
+    if (PyType_Ready(&Imaging_Type) < 0)
+        return -1;
+
+    return 0;
+}
+
+#if PY_VERSION_HEX >= 0x03000000
+PyMODINIT_FUNC PyInit_PilLiteExt(void) {
+    PyObject* m;
+
+    static PyModuleDef module_def = {
+        PyModuleDef_HEAD_INIT,
+        "PilLiteExt",       /* m_name */
+        NULL,               /* m_doc */
+        -1,                 /* m_size */
+        functions,          /* m_methods */
+    };
+
+    m = PyModule_Create(&module_def);
+
+    if (setup_module(m) < 0)
+        return NULL;
+
+    return m;
+}
+#else
+PyMODINIT_FUNC initPilLiteExt(void)
+{
+    PyObject* m = Py_InitModule("PilLiteExt", functions);
+    setup_module(m);
+}
+#endif
